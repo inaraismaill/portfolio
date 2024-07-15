ef95ce906411919554e91c70a0d49b3266064173
inaraismaill <mi7q3xpdl@code.edu.az>
2024-07-15 10:12:36 +0400

    fixed send mail

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,57 +22,28 @@ if (navToggle) {
         })
     })
 }
-var submitButton = document.getElementById("submitBtn");
 
-submitButton.addEventListener("click", function () {
-    (function () {
-        emailjs.init({
-            publicKey: "e7o9lKvXcEiiie95s",
-        });
-    })();
+
+function sendMail(event) {
+    event.preventDefault();
     var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
     };
-
-    const serviceID = "service_j6i0zpc";
+  
+    const serviceID = "service_yn89u34";
     const templateID = "template_1vd7xc6";
-
-    emailjs.send(serviceID, templateID, params)
-        .then(res => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("Your message sent successfully!!")
-        })
-        .catch(err => console.log(err));
-});
-
-
-
-
-// function sendMail(e) {
-
-//     var params = {
-//         name: document.getElementById("name").value,
-//         email: document.getElementById("email").value,
-//         message: document.getElementById("message").value,
-//     };
-
-//     const serviceID = "service_j6i0zpc";
-//     const templateID = "template_1vd7xc6";
-
-//     emailjs.send(serviceID, templateID, params)
-//     .then(res=>{
-//         document.getElementById("name").value = "";
-//         document.getElementById("email").value = "";
-//         document.getElementById("message").value = "";
-//         console.log(res);
-//         alert("Your message sent successfully!!")
-
-//     })
-//     .catch(err=>console.log(err));
-//     e.preventDefault();
-// }
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
